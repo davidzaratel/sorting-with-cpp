adfe10ae1b23a7327ff3d759db91cc0d8c363d57
David Zarate <dzlopez35@gmail.com>
2021-06-28 17:28:50 -0500

    Bubble Sort finished

--- main.cpp
+++ main.cpp
@@ -20,6 +20,26 @@ void exchangeSort(int list[10]){
   }
 }
 
+//Complexity O(n) best case and O(n^2) in worst cases
+void bubbleSort(int list[10]){
+  bool flag = true;
+  //this for will execute n-1 times, being n the size of the array and it will only repeat if there's an exchange of places
+  //if there isn't it will mean that the array is already sorted
+  for (int i = 0; i < 9 && flag; i++) {
+    flag = false;
+    //this for will execute n-1-i times, being n the size of the array and i the number of repetitions.
+    //that will mean that every time that the first for repeats, the comparisons will be less
+    for (int j = 0 ; j < 9 - i; j++) {
+      if (list[j+1] > list[j+1]) {
+        int aux;
+        aux = list[j+1];
+        list[j+1] = list[j];
+        list[j] = aux;
+      }
+    }
+  }
+}
+
 void printList(int list[10]){
   for (int i = 0; i < 10 ; i++)
     cout<<list[i]<<" ";
@@ -28,8 +48,12 @@ void printList(int list[10]){
 
 int main(){
     int list[10] = {9, 500, 1, 8, 4, 100, 99, 61, 40, 2};
+    int list2[10] = {9, 500, 1, 8, 4, 100, 99, 61, 40, 2};
     printList(list);
     exchangeSort(list);
     printList(list);
+    printList(list2);
+    exchangeSort(list2);
+    printList(list2);
 
 }
